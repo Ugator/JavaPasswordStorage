f8742ebc38e5211027d510a0729cf89be9ba2d70
Ugator <github@florianlosch.de>
2018-12-18 12:23:21 +0100

    sonarcloud
    aaa

---
+++ static_analyze.bat
@@ -0,0 +1,5 @@
+mvn sonar:sonar \
+  -Dsonar.projectKey=Ugator_JavaPasswordStorage \
+  -Dsonar.organization=ugator-github \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=1f38409868b7540b932bce8bf212a15f945356e3
